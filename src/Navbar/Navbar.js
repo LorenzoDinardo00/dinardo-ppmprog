87c6cedb5682fc42e0dcd78d25a3198ab83cbfd6
lorenzo <lorenzo.dinardo@stud.unifi.it>
2024-05-08 11:15:36 +0200

    terminato il menù sezione !! inizio titolo icona

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,126 @@
 import React from 'react';
-import SubNavbar from './SubNavbar.js';
-function Navbar(){
+import SubNavbarcss from './Navbar.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function Navbar() {
   return (
-    <div className = "navbar">
-      <SubNavbar />
-      <h1></h1>
-    </div>
+    <nav className="navbar navbar-dark fixed-top">
+      <div className="container1menu">
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
+                aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          <a className={"sezione"} href={"#"}>Sezioni</a>
+        </button>
+        <div className={"CorriereTitle"}><h1>Corriere della Sera</h1>
+          <div className={"lastupdate"}><h2>Aggiornato alle 21:33 </h2></div>
+        </div>
+
+        {/* Qui aggiungi il tuo nuovo menù a destra */}
+        <div className="ms-auto">
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+              Edizioni Locali
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+              Servizi
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className="offcanvas offcanvas-start text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar"
+             aria-labelledby="offcanvasDarkNavbarLabel">
+          <nav className="navbar">
+            <div className="container-fluid text-center navbar-centered">
+              <button type="button" className="btn-close" aria-label="Close" data-bs-dismiss="offcanvas"></button>
+              <h3 className="titoloSub"> Corriere della sera</h3>
+            </div>
+          </nav>
+          <div className="input-group mt-3">
+            <input className="form-control" type="search" placeholder="Cerca in Corriere" aria-label="Search"/>
+            <span className="input-group-text">
+              <i className="fa fa-search"></i>
+            </span>
+          </div>
+          <div className="container-popolari">
+            <div className="title-popolari">Più popolari</div>
+            <div className="columns">
+              <div className="column">
+                <a href="#" className="link">UCRAINA RUSSIA</a>
+                <a href="#" className="link">ISRAELE</a>
+                <a href="#" className="link">MELONI</a>
+                <a href="#" className="link">BCE</a>
+                <a href="#" className="link">SINNER</a>
+                <a href="#" className="link">PRIMA PAGINA</a>
+              </div>
+              <div className="column">
+                <a href="#" className="link">SERIE A</a>
+                <a href="#" className="link">FORMULA 1</a>
+                <a href="#" className="link">CINEMA</a>
+                <a href="#" className="link">PODCAST DAILY</a>
+                <a href="#" className="link">ELEZIONI</a>
+                <a href="#" className="link">OROSCOPO FOX</a>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div className="container-inevidenza">
+            <div className="title-inevidenza">In evidenza</div>
+            <div className="columns">
+              <div className="column">
+                <a href="#" className="link">UCRAINA RUSSIA</a>
+                <a href="#" className="link">ISRAELE</a>
+                <a href="#" className="link">MELONI</a>
+                <a href="#" className="link">BCE</a>
+                <a href="#" className="link">SINNER</a>
+                <a href="#" className="link">PRIMA PAGINA</a>
+              </div>
+              <div className="column">
+                <a href="#" className="link">SERIE A</a>
+                <a href="#" className="link">FORMULA 1</a>
+                <a href="#" className="link">CINEMA</a>
+                <a href="#" className="link">PODCAST DAILY</a>
+                <a href="#" className="link">ELEZIONI</a>
+                <a href="#" className="link">OROSCOPO FOX</a>
+              </div>
+            </div>
+          </div>
+          <nav className="sub-navbar">
+            <ul className="nav-list">
+              <li className="nav-item">Podcast <span className="icona">></span></li>
+              <li className="nav-item">Dizionari <span className="icona">></span></li>
+
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className={"meteo-firenze"}><h1>Meteo: Firenze 15°<i className="bi bi-cloud-drizzle-fill"></i></h1></div>
+      <div className="button-group"> {/* Aggiunta del contenitore per i bottoni */}
+        <button className="subscribe-button"><i className="bi bi-brightness-low-fill"></i>ABBONATI</button>
+        <a href={"access-button"} className={"access-button"}><i class="bi bi-person-circle"></i>Accedi</a>
+      </div>
+    </nav>
   );
 }
- export default Navbar;
+
+
+export default Navbar;
