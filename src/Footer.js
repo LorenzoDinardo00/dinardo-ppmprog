6dd8209c6680c45cf405ccbad7d9eaec0a10add8
lorenzo <lorenzo.dinardo@stud.unifi.it>
2024-05-21 10:01:49 +0200

    Fixato errori e preparato per l'anteprima ufficiale

--- src/Footer.js
+++ src/Footer.js
@@ -6,20 +6,20 @@ function Footer() {
       <footer>
         <h1 className={"FooterTitle"}>Corriere della Sera-replica</h1>
         <div className="footer-top">
-          <a href="#">Abbonati a Corriere della Sera</a> |
-          <a href="#">Gazzetta</a> |
-          <a href="#">El Mundo</a> |
-          <a href="#">Marca</a> |
-          <a href="#">RCS Mediagroup</a> |
-          <a href="#">Fondazione Corriere</a> |
-          <a href="#">Fondazione Cutuli</a> |
-          <a href="#">Quimamme</a> |
-          <a href="#">OFFERTE CORRIERE STORE</a> |
-          <a href="#">Buonpertutti</a>
+          <a href="">Abbonati a Corriere della Sera</a> |
+          <a href="">Gazzetta</a> |
+          <a href="">El Mundo</a> |
+          <a href="">Marca</a> |
+          <a href="">RCS Mediagroup</a> |
+          <a href="">Fondazione Corriere</a> |
+          <a href="">Fondazione Cutuli</a> |
+          <a href="">Quimamme</a> |
+          <a href="">OFFERTE CORRIERE STORE</a> |
+          <a href="">Buonpertutti</a>
         </div>
         <div className="footer-bottom">
-          <a href="#">Codici Sconto</a> |
-          <a href="#">Corso di Inglese - Francese</a>
+          <a href="">Codici Sconto</a> |
+          <a href="">Corso di Inglese - Francese</a>
           <p>Copyright 2024 © Lorenzo Dinardo Tutti i diritti sono riservati | Per la pubblicità: Lorenzo Dinardo Media
             OK - Direzione Pubblicità<br/>
            <br/>
